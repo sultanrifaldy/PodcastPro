ffea2710de481bd3414c56897b948ddc76bdfd66
Sultan Rifaldy <sultanrifaldy@gmail.com>
2023-05-23 14:10:59 +0700

    setup

---
+++ PodcastPro/PodcastPro/Modules/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastPro
+//
+//  Created by Sultan Rifaldy on 23/05/23.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
