ceacbbc73bfe12b8ca83686cb997887cbddade87
Sultan Rifaldy <sultanrifaldy@gmail.com>
2023-05-24 15:10:31 +0700

    some changes

--- PodcastPro/PodcastPro/Modules/Home/HomeViewController.swift
+++ PodcastPro/PodcastPro/Modules/Home/HomeViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setup()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setup() {
+        tableView.dataSource = self
     }
-    */
 
+}
+
+
+extension HomeViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
